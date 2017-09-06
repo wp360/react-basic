1e48711affd43a2c9278f2bb59e8e3cf3fd00789
wp360 <2897406527@qq.com>
2017-09-06 16:01:18 +0800

    add file

--- src/components/app.js
+++ src/components/app.js
@@ -5,27 +5,27 @@ export default class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      counter: 0,
+      textValue: '',
       value: ''
     };
   }
 
   textEntered = (event) => {
-    console.log('text：', event.target.value);
     this.setState({ value: event.target.value });
+  }
+
+  changeText = () => {
+    this.setState({ textValue: this.state.value });
   }
 
   render(){
     return(
       <div>
-        <p>
-          <span className="btn btn-default">{this.state.counter}</span>
-          <Button name={'加1'} clicked={()=> this.setState({counter: this.state.counter + 1})} />
-        </p>
-        <div>
-          <input type="text" value={this.state.value} onChange={this.textEntered} />
-          <p>{this.state.value}</p>
-        </div>
+          <p>
+            <input type="text" value={this.state.value} onChange={this.textEntered} />
+            <span>输入文字： {this.state.textValue}</span>
+          </p>
+          <Button name={'点击'} clicked={this.changeText} />
       </div>
     );
   }
